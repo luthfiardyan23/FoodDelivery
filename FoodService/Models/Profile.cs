806b1dff114251065b5f6824853abb54bfd69592
luthfiardyan23 <lardyan45@gmail.com>
2022-05-17 10:06:09 +0700

    Scaffold pak....

---
+++ FoodService/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodService.Models
+{
+    public partial class Profile
+    {
+        public int Id { get; set; }
+        public int UseId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Addres { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+
+        public virtual User Use { get; set; } = null!;
+    }
+}
